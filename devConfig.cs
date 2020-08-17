12749b06d51985f6a024947a54db528c236ec185
StarrKiss <gregorystarr00@gmail.com>
2020-08-16 19:37:06 -0500

    testing

---
+++ devConfig.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace discBot
+{
+    public struct  devConfig
+    {
+        [JsonProperty("id")]
+        public string id;
+        
+
+
+        
+    }
+    
+    
+}
